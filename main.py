23baac8a26f14b5c49229c31ce6735012a198852
OkciD <yury.baykov@yandex.ru>
2017-07-18 02:13:30 +0300

    document.xml is now deleted after work

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import zipfile
+import os
+import shutil
 
 
 def extract_document_xml(docx_filename):
     archive = zipfile.ZipFile(docx_filename);
-
+    archive.extract("word/document.xml");
     archive.close();
+
+
+def delete_document_xml():
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "word");
+    shutil.rmtree(path)
 
 
 def main():
     filename = input("Введите имя файла: ");
     extract_document_xml(filename);
+    # delete_document_xml();
+
+
+main();
