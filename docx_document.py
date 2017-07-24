7821c481b596cadb974104479b3f60ed605f8e59
OkciD <yury.baykov@yandex.ru>
2017-07-25 02:48:27 +0300

    app.xml root cleaning added

--- docx_document.py
+++ docx_document.py
@@ -10,6 +10,7 @@ class DocxDocument:
         self.document_xml_path = "word/document.xml";
         self.app_xml_path = "docProps/app.xml";
         self.extract_xmls();
+        self.clean_app_xml_root();
         self.document_xml = etree.parse(self.document_xml_path);
         self.app_xml = etree.parse(self.app_xml_path);
 
@@ -18,6 +19,14 @@ class DocxDocument:
         archive.extract(self.document_xml_path);
         archive.extract(self.app_xml_path)
         archive.close();
+
+    def clean_app_xml_root(self):
+        app_xml_file = open(self.app_xml_path, "r");
+        app_xml_file_contents = app_xml_file.read().replace('<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">', "<Properties>");
+        app_xml_file.close();
+        app_xml_file = open(self.app_xml_path, "w");
+        app_xml_file.write(app_xml_file_contents);
+        app_xml_file.close();
 
     def __del__(self):
         path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "word");
